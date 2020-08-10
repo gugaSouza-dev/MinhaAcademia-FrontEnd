981b53201d81eefe551f121962bb03551dc2e86a
ItsJustAGust <gustavo_souza.dev@outlook.com>
2020-08-10 16:26:05 -0300

    Continuando tela de login/registro

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import swal from 'sweetalert';
+import validator from 'email-validator';
 import "./Login.css";
 import login from '../../assets/img/login.jpg';
 import registro from '../../assets/img/registro.jpg';
@@ -11,14 +13,41 @@ function toggleForm() {
     container.classList.toggle('active')
 }
 
+
 export default class Login extends Component {
     constructor() {
         super();
 
         this.state = {
             email: '',
-            senha: ''
+            senha: '',
+            // confSenha: ''
         }
+    }
+
+    cadastroFeito() {
+        swal({
+            title: "Boa!",
+            text: "Sua Conta Foi Criada",
+            icon: "success",
+            timer: 3000
+        });
+    }
+
+    emailJaExiste() {
+        swal({
+            text: "Email ja cadastrado",
+            icon: "error",
+            timer: 3000
+        });
+    }
+
+    emailInvalido(){
+        swal({
+            text: "Email inválido",
+            icon: "error",
+            timer: 3000
+        });
     }
 
     atualizaEstadoEmail(event) {
@@ -29,6 +58,10 @@ export default class Login extends Component {
         this.setState({ senha: event.target.value });
     }
 
+    // atualizaEstadocConfSenha(event) {
+    //     this.setState({ confSenha: event.target.value });
+    // }
+
     efetuaLogin(event) {
         event.preventDefault();
 
@@ -38,30 +71,50 @@ export default class Login extends Component {
         })
             .then(data => {
                 localStorage.setItem("academia", data.data.token);
-                // this.props.history.push('/consultaslistar');
                 console.log(data);
             })
             .catch(erro => {
                 console.log(erro);
             })
+            .finally(data =>{
+                this.props.history.push('/');
+            })
     }
 
-    cadastraUsuario(event) {
+
+    cadastraUsuario = (event) => {
         event.preventDefault();
+        if (this.state.senha.length < 7) {
+            document.getElementById('senhaMensagem').style.color = 'rgba(255, 75, 75, 0.686)';
+            document.getElementById('senhaMensagem').innerHTML = 'Senha precisa ter mais que 7 caracteres';
 
-        axios.post('http://localhost:3333/registro', {
-            email: this.state.email,
-            senha: this.state.senha
-        })
-            .then(data => {
-                localStorage.setItem("academia", data.data.token);
-                console.log(data);
-            })
-            .catch(erro => {
-                console.log(erro);
-            })
+        } else {
 
+            if (validator.validate(this.state.email)) {
+
+                axios.post('http://localhost:3333/registro', {
+                    email: this.state.email,
+                    senha: this.state.senha
+                })
+                    .then(data => {
+                        this.cadastroFeito();
+                        localStorage.setItem("academia", data.data.token);
+                        console.log(data);
+                    })
+                    .catch(erro => {
+                        this.emailJaExiste();
+                        console.log(erro);
+                    })
+                    .finally(data =>{
+                        this.props.history.push('/');
+                    })
+            } else {
+                console.log("email invalido")
+                this.emailInvalido();
+            }
+        }
     }
+
 
     render() {
         return (
@@ -69,7 +122,7 @@ export default class Login extends Component {
                 <div className="container">
                     <div className="login">
                         <div className="imgContainer">
-                            <img className="img" src={login} alt="Imagem meramente ilustrativa ao lado do login"/>
+                            <img className="img" src={login} alt="Imagem meramente ilustrativa ao lado do login" />
                         </div>
                         <div className="formContainer">
                             <form id="form" onSubmit={this.efetuaLogin.bind(this)}>
@@ -106,31 +159,43 @@ export default class Login extends Component {
 
                             <form id="form" onSubmit={this.cadastraUsuario.bind(this)}>
                                 <div className="logo">
-                                    <img className="logoImg" src={logo} alt="Logo em forma de camaleao"/>
+                                    <img className="logoImg" src={logo} alt="Logo em forma de camaleao" />
                                 </div>
                                 <h2>Crie sua conta</h2>
                                 <input
                                     placeholder="Email"
                                     type="text"
+                                    required={true}
                                     value={this.state.email}
                                     onChange={this.atualizaEstadoEmail.bind(this)}
-                                /> 
+                                />
                                 <input
                                     placeholder="Senha"
                                     type="password"
+                                    required={true}
                                     value={this.state.senha}
+                                    minLength={7}
                                     onChange={this.atualizaEstadoSenha.bind(this)}
                                 />
+                                <div className="senhaMsg">
+                                    <span id='senhaMensagem'></span>
+                                </div>
+                                {/* 
+                                <input
+                                    placeholder="Confirme senha"
+                                    type="password"
+                                    required={true}
+                                    value={this.state.confSenha}
+                                    onChange={this.atualizaEstadocConfSenha.bind(this)}
+                                /> */}
                                 <input type="submit" value="Entrar" />
                                 <p className="signUp">Já possui conta? <a onClick={toggleForm}>  Login</a></p>
                             </form>
                         </div>
                         <div className="imgContainer">
-                            <img className="img" src={registro} alt="Imagem meramente ilustrativa ao lado do registro"/>
+                            <img className="img" src={registro} alt="Imagem meramente ilustrativa ao lado do registro" />
                         </div>
                     </div>
-
-
                 </div>
             </section>
 
